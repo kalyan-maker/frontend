5209e1e4e5a999d83c885522881fa55ef3f0cd1a
kalyan-maker <kalyanraju531@gmail.com>
2021-11-29 12:17:48 +0530

    blog app

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="headersection">
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">Mern app blog app</span>
+      </div>
+    </div>
+    </div>
+  );
+}
